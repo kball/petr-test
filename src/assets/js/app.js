4c795c373316e7276a296cbf4fdebd953e44366c
Kevin Ball <kmball11@gmail.com>
2017-08-23 11:24:38 -0700

    add nette.ajax.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,9 @@ import $ from 'jquery';
 import whatInput from 'what-input';
 
 window.$ = $;
+window.jQuery = $;
+
+require('nette.ajax.js');
 
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
